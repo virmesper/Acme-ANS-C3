9fb4372fda94789b45042b10c8430269e199dcea
Paula <pausangom1@alum.us.es>
2025-02-25 21:54:06 +0100

    fix: Update Tracking Log entity

--- src/main/java/acme/entities/S4/TrackingLog.java
+++ src/main/java/acme/entities/S4/TrackingLog.java
@@ -1,68 +1,64 @@
 
 package acme.entities.S4;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PastOrPresent;
-import javax.validation.constraints.Size;
 
 import acme.client.components.basis.AbstractEntity;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-@Table(name = "tracking_logs")
-
 public class TrackingLog extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
-
 	private static final long	serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
 
-	// Momento de la última actualización del seguimiento 
-	@PastOrPresent
-	@NotNull
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
 	@Column(nullable = false)
-	private LocalDateTime		lastUpdateMoment;
+	private Date				lastUpdateMoment;
 
-	// Paso actual en el procedimiento de resolución
-	@NotBlank
-	@Size(max = 50)
-	@Column(length = 50, nullable = false)
-	private String				step;
+	@Mandatory
+	@ValidString(max = 50)
+	@Column(nullable = false, length = 50)
+	private String				currentStep;
 
+	@Mandatory
 	@Min(0)
 	@Max(100)
 	@Column(nullable = false)
 	private Integer				resolutionPercentage;
 
-	// Indica si el reclamo ha sido finalmente aceptado o no
-	@NotNull
+	@Mandatory
 	@Column(nullable = false)
 	private Boolean				finalDecision;
 
-	// Motivo de rechazo o compensación ofrecida
-	@Size(max = 255)
-	@Column(length = 255, nullable = true)
-	private String				resolutionDetail;
+	@Mandatory
+	@ValidString(max = 255)
+	@Column(nullable = false, length = 255)
+	private String				resolutionDetails;
 
 	// Relationships ----------------------------------------------------------
 
-	// Reclamo al que pertenece este seguimiento 
+	@Mandatory
+	@Valid
 	@ManyToOne(optional = false)
-	@JoinColumn(name = "claim_id", nullable = false)
-	private Claim				claim;
+	private Claim				claim; // Relación con la reclamación a la que pertenece
 }
