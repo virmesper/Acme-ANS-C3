faff0f96351a00ee8f22a5c47b1cad7d63ecca30
Virginia Mesa Pérez <virmesper@alum.us.es>
2025-02-26 18:27:23 +0100

    Update Entities

--- src/main/java/acme/entities/Group/Aircraft.java
+++ src/main/java/acme/entities/Group/Aircraft.java
@@ -3,11 +3,10 @@ package acme.entities.Group;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidNumber;
@@ -28,6 +27,7 @@ public class Aircraft extends AbstractEntity {
 
 	@Mandatory
 	@ValidString(max = 50)
+	@Automapped
 	private String				model;
 
 	@Mandatory
@@ -37,19 +37,22 @@ public class Aircraft extends AbstractEntity {
 
 	@Mandatory
 	@ValidNumber
+	@Automapped
 	private Integer				capacity;
 
 	@Mandatory
 	@ValidNumber(min = 2000, max = 50000)
+	@Automapped
 	private Double				cargoweight;
 
 	@Mandatory
-	@Enumerated(EnumType.STRING)
 	@Valid
+	@Automapped
 	private Status				status;
 
 	@Optional
 	@ValidString(max = 255)
+	@Automapped
 	private String				details;
 
 	// Derived attributes -----------------------------------------------------
--- src/main/java/acme/entities/S2/Booking.java
+++ src/main/java/acme/entities/S2/Booking.java
@@ -5,18 +5,18 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.Valid;
-import javax.validation.constraints.Past;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
-import acme.client.components.validation.ValidCreditCard;
+import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidString;
 import acme.realms.Customer;
@@ -42,24 +42,23 @@ public class Booking extends AbstractEntity {
 	private String				locatorCode;
 
 	@Mandatory
-	@Past
-<<<<<<< HEAD
-	private LocalDateTime		purchaseMoment;
-=======
+	@ValidMoment(past = true)
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date				purchaseMoment;
->>>>>>> master
 
 	@Mandatory
-	@Enumerated(EnumType.STRING)
+	@Valid
+	@Automapped
 	private TravelClass			travelClass;
 
 	@Mandatory
 	@ValidMoney
+	@Automapped
 	private Money				price;
 
 	@Optional
-	@ValidCreditCard
+	@ValidString(pattern = "^[0-9]{4}$")
+	@Automapped
 	private String				lastCardDigits;
 
 	// Derived attributes -----------------------------------------------------
