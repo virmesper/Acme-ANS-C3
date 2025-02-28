23a4ff013949a82d3a1be908184ce322d3d71130
marioreyesapresa <marreyapr@alum.us.es>
2025-02-28 13:09:24 +0100

    feat: add service entity

--- src/main/java/acme/entities/S5/Technician.java
+++ src/main/java/acme/entities/S5/Technician.java
@@ -3,12 +3,12 @@ package acme.entities.S5;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.validation.constraints.Min;
 
 import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,7 +16,6 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(name = "technicians")
 public class Technician extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
@@ -27,30 +26,30 @@ public class Technician extends AbstractEntity {
 
 	@Mandatory
 	@ValidString(pattern = "^[A-Z]{2,3}\\d{6}$")
-	@Column(unique = true, nullable = false)
+	@Column(unique = true)
 	private String				licenseNumber;
 
 	@Mandatory
 	@ValidString(pattern = "^\\+?\\d{6,15}$")
-	@Column(nullable = false)
+	@Automapped
 	private String				phoneNumber;
 
 	@Mandatory
 	@ValidString(max = 50)
-	@Column(nullable = false, length = 50)
+	@Automapped
 	private String				specialisation;
 
 	@Mandatory
-	@Column(nullable = false)
+	@Automapped
 	private boolean				healthTestPassed;
 
 	@Mandatory
-	@Min(0)
-	@Column(nullable = false)
+	@ValidNumber(min = 0)
+	@Automapped
 	private Integer				yearsOfExperience;
 
 	@Optional
 	@ValidString(max = 255)
-	@Column(length = 255)
+	@Automapped
 	private String				certifications;
 }
