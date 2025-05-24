b35ece26e7dbd99bbe8c15e85fb390963cc613f5
Virginia Mesa Pérez <virmesper@alum.us.es>
2025-05-24 16:15:38 +0200

    D04

--- src/main/java/acme/entities/S2/BookingRecord.java
+++ src/main/java/acme/entities/S2/BookingRecord.java
@@ -2,7 +2,9 @@
 package acme.entities.S2;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
@@ -13,6 +15,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(indexes = {
+	@Index(columnList = "booking_id"), @Index(columnList = "passenger_id")
+})
 public class BookingRecord extends AbstractEntity {
 	// Serialisation version --------------------------------------------
 
